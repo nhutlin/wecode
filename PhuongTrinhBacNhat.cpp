2741f72ef04a4816d6dc96696a3600cb1087ab82
Nhựt Linh <97160162+nhutlin231@users.noreply.github.com>
2022-02-23 09:02:32 +0700

    Update PhuongTrinhBacNhat.cpp

--- PhuongTrinhBacNhat.cpp
+++ PhuongTrinhBacNhat.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 	float a, b;
-	cin >> a >> b;
+	cout << "Phuong trinh bac nhat co dang: ax + b = 0" << endl;
+	cout << "Nhap he so a: ";
+	cin >> a;
+	cout << "Nhap he so b: ";
+	cin >> b;
 	if (a == 0 && b == 0) {
-		cout << "Vo so nghiem";
+		cout << "Phuong trinh vo so nghiem";
 	}
 	else if (a == 0) {
-		cout << "Vo nghiem";
+		cout << "Phuong trinh vo nghiem";
 	}
 	else if (b == 0) {
-	cout << "0";
+	cout << "x = 0";
     }
 	else {
-		cout << -b / a;
+		cout << "x = " << -b / a;
 	}
 	return 0;
 }
